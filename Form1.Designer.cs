aa842d652e8d7d2dcace00e5a33c32df95892134
yas-mine-az <yasmine.arfa.zahira@mail.ugm.ac.id>
2023-12-12 09:30:37 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Responsi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.labelDep = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.dropdownDep = new System.Windows.Forms.ComboBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.IDdept = new System.Windows.Forms.GroupBox();
+            this.karyawan = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.karyawan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Location = new System.Drawing.Point(71, 29);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(27, 13);
+            this.labelLogo.TabIndex = 0;
+            this.labelLogo.Text = "logo";
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(16, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(49, 50);
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Location = new System.Drawing.Point(12, 87);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(91, 13);
+            this.labelNama.TabIndex = 2;
+            this.labelNama.Text = "Nama Karyawan :";
+            // 
+            // labelDep
+            // 
+            this.labelDep.AutoSize = true;
+            this.labelDep.Location = new System.Drawing.Point(12, 116);
+            this.labelDep.Name = "labelDep";
+            this.labelDep.Size = new System.Drawing.Size(89, 13);
+            this.labelDep.TabIndex = 3;
+            this.labelDep.Text = "Dep. Karyawan : ";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(109, 84);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(100, 20);
+            this.tbNama.TabIndex = 4;
+            this.tbNama.TextChanged += new System.EventHandler(this.tbNama_TextChanged);
+            // 
+            // dropdownDep
+            // 
+            this.dropdownDep.FormattingEnabled = true;
+            this.dropdownDep.Location = new System.Drawing.Point(109, 113);
+            this.dropdownDep.Name = "dropdownDep";
+            this.dropdownDep.Size = new System.Drawing.Size(121, 21);
+            this.dropdownDep.TabIndex = 5;
+            this.dropdownDep.SelectedIndexChanged += new System.EventHandler(this.dropdownDep_SelectedIndexChanged);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(16, 155);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 6;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(183, 155);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(350, 155);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // IDdept
+            // 
+            this.IDdept.Location = new System.Drawing.Point(263, 12);
+            this.IDdept.Name = "IDdept";
+            this.IDdept.Size = new System.Drawing.Size(153, 122);
+            this.IDdept.TabIndex = 9;
+            this.IDdept.TabStop = false;
+            this.IDdept.Text = "ID Departemen";
+            // 
+            // karyawan
+            // 
+            this.karyawan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.karyawan.Location = new System.Drawing.Point(16, 199);
+            this.karyawan.Name = "karyawan";
+            this.karyawan.Size = new System.Drawing.Size(401, 150);
+            this.karyawan.TabIndex = 10;
+            this.karyawan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabeldata_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(436, 371);
+            this.Controls.Add(this.karyawan);
+            this.Controls.Add(this.IDdept);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.dropdownDep);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.labelDep);
+            this.Controls.Add(this.labelNama);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.labelLogo);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.karyawan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.Label labelDep;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.ComboBox dropdownDep;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.GroupBox IDdept;
+        private System.Windows.Forms.DataGridView karyawan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Responsi2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost; Port=5434; Username=postgres; Password=postgres; Database=Responsi2";
+    
+        public DataTable df;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+
+
+        public
+
+        private void Form1_Load_1(object sender, EventArgs e) => conn = new NpgsqlConnection(connstring);
+
+        private void tbNama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dropdownDep_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+          try
+            {
+                conn.Open();
+                sql = @"select * from st_insert(:_name, :_depname)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_name", tbNama.Text);
+                cmd.Parameters.AddWithValue("_depname", dropdownDep.Text);
+                if((int)cmd.ExecuteString()==1)
+                {
+                    MessageBox.Show("Data Users berhasil diinputkan", "Yey", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" +  ex.Message, "Insert FAIL!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                r = dgv.Rows[e.RowIndex];
+                tbNama = r.Cells["_name"].Value.ToString();
+                dropdownDep = r.Cells["_depname"].Value.ToString() ;
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon pilih baris data yang akan diupdate", "Warning!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Apakah benar anda ingin menghapus data" + r.Cells["_name"],Value.ToString() + "?", "Hapus data terkonfirmasi", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+
+            }
+        }
+
+        private void tabeldata_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
